ae261f3e10605724a517c890ea5d3ab65669d3a9
vaishalir09 <147117304+vaishalir09@users.noreply.github.com>
2025-01-14 10:13:11 +0000

    operations conversions config

--- .devcontainer/01_Basics/03_conversionoperations.js
+++ .devcontainer/01_Basics/03_conversionoperations.js
@@ -25,3 +25,43 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+// ********************  Operations ****************
+
+let value = 3
+let negvalue= -value
+console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1= "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
+
+console.log((3 + 4)* 5 % 3);
+//console.log(+true);//O/p: 1
+//console.log(true+);// Show Error
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// Link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
