6a05bfd78c57e227076115e097b323a21d78f1b0
vaishalir09 <147117304+vaishalir09@users.noreply.github.com>
2025-01-16 10:30:11 +0000

    String

---
+++ .devcontainer/01_Basics/05_String.js
@@ -0,0 +1,34 @@
+const name = "hitesh";
+const repoCount = 50 
+
+//console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount} `);//say , String Interpolation
+
+const gameName = new String('hiteshhc');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);//op: hite
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);//ite
+
+const newStringOne = "  hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hite// sh%20choudhary";
+
+console.log(url.replace('%20', '_'))
+
+console.log(url.includes('sundar'));
+console.log(gameName.split('_'));
