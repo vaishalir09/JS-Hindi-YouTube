260ecf31431db6e8b7015c0083e80d95cb78f9c8
vaishalir09 <147117304+vaishalir09@users.noreply.github.com>
2025-01-26 05:52:26 +0000

    Name Change

---
+++ devcontainer/02_Basics/03_Objects.js
@@ -0,0 +1,17 @@
+// Singleton 
+// Object.create 
+
+// Object literals
+const JsUser = {
+   name: "Vaishali",
+   age: +19,
+   location: "Jaipur",
+   email: "vaishalir888@gmail.com",
+   isLoggedIn: false,
+   lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+
+
