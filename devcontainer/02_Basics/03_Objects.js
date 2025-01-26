375073f1b8e290e3e60e6923bdd413798a2d646b
vaishalir09 <147117304+vaishalir09@users.noreply.github.com>
2025-01-26 06:27:38 +0000

    Objects

--- devcontainer/02_Basics/03_Objects.js
+++ devcontainer/02_Basics/03_Objects.js
@@ -2,8 +2,12 @@
 // Object.create 
 
 // Object literals
+
+const mySym = Symbol("key1 ")
 const JsUser = {
    name: "Vaishali",
+   "full name": "Vaishali Rathod",
+   [mySym]: "mykey1",//this syntax refers to Symbol use in objects as a keys format
    age: +19,
    location: "Jaipur",
    email: "vaishalir888@gmail.com",
@@ -13,5 +17,21 @@ const JsUser = {
 
 console.log(JsUser.email)
 console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])//Print Symbol
+console.log(typeof JsUser.mySym)
 
+JsUser.email = "sonamr2309@gmail.com"
+//Object.freeze(JsUser)
+JsUser.email = "sonamr888@gmail.com"
+console.log(JsUser);
 
+JsUser.greeting = function(){
+   console.log("Hello Js User")
+}
+
+JsUser.greetingtwo = function(){
+   console.log(`Hello JS User, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingtwo());
