aeeda150e34702c893edff809ebdcae6f65da68f
vaishalir09 <147117304+vaishalir09@users.noreply.github.com>
2025-01-29 17:12:35 +0000

    object

---
+++ devcontainer/02_Basics/04_Objects.js
@@ -0,0 +1,28 @@
+// const tinderUser = new Object()
+const tinderUser ={}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1)
+
