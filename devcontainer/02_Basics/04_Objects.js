4553c11c8c9f949abab7e775c63a7269e90acb59
vaishalir09 <147117304+vaishalir09@users.noreply.github.com>
2025-02-23 08:39:38 +0000

    object

--- devcontainer/02_Basics/04_Objects.js
+++ devcontainer/02_Basics/04_Objects.js
@@ -50,8 +50,31 @@
  console.log(Object.keys(tinderUser));    
  console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
-
-
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+  }
+   
+  // course.courseInstructor
+  
+  const {courseInstructor: instructor} = course
+  
+  //console.log(courseInstructor);
+  console.log(instructor);
+  
+  //{
+    //  "name": "hitesh",
+    //  "courseName": "js in hindi",
+    //  "price": "free"
+  //}
+  
+  [
+      {},
+      {},
+      {}
+  ]
+   
 console.log(tinderUser.hasOwnProperty('isLogged'));
 
 
